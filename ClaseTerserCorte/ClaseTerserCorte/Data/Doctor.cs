f12a11a140f5c7d34fb7ffaa2d8bf3574bf074b4
Juan Sebastian Pardo Parra <99051327+SebassPP@users.noreply.github.com>
2022-05-28 22:13:24 -0500

    Parte 1

---
+++ ClaseTerserCorte/ClaseTerserCorte/Data/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ClaseTerserCorte.Data
+{
+    public class Doctor
+    {
+        [Key]
+        public int IdDoctor { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Departamento { get; set; }
+        public int Descripcion { get; set; }
+        public string FechaNacimiento { get; set; }
+    }
+}
---
+++ ClaseTerserCorte/ClaseTerserCorte/Data/Paciente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ClaseTerserCorte.Data
+{
+    public class Paciente
+    {
+        [Key]
+        public int IdPaciente { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Departamento { get; set; }
+        public string Descripcion { get; set; }
+        public string FechaNacimiento { get; set; }
+    }
+}
